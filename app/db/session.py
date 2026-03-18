fcc83babce619b86822bc5fa015ef0241861fff0
saniabduljabbar619-create <saniabduljabbar619@gmail.com>
2026-03-18 23:26:52 +0100

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -9,6 +9,7 @@ from app.core.config import settings
 engine = create_engine(
     settings.DATABASE_URL,
     pool_pre_ping=True,
+    connect_args={"ssl": {"ssl_disabled": False}},
 )
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
