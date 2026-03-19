24e5cdbce453750eb0e5033a89749ea28042228d
saniabduljabbar619-create <saniabduljabbar619@gmail.com>
2026-03-19 12:39:05 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # APP INIT (ONLY ONCE)
 # --------------------------------------------------
 # main.py
-app = FastAPI(title=settings.APP_NAME, redirect_slashes=False)
+app = FastAPI(title=settings.APP_NAME)
 
 # --------------------------------------------------
 # CORS (ALLOW ALL FOR NOW)
